f9149621b0594fb30a81fb690a6fc0db2c946a90
Shota Mestiashvili <shota.mestiashvili@caucasusauto.com>
2024-04-12 17:57:59 +0400

    initial commit

---
+++ src/AuctionService/Entities/Auction.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AuctionService.Entities;
+
+
+public class Auction
+{
+    public Guid Id { get; set; }
+    public int ReservePrice { get; set; } = 0;
+    public string Seller { get; set; }
+    public string? Winner { get; set; }
+    public int? CurrentHigh { get; set; }
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    public DateTime UpdateAt { get; set; } = DateTime.UtcNow;
+    public DateTime AuctionEnd { get; set; }
+    public Status Status { get; set; }
+    public Item Item { get; set; }
+}
